bdda517e88890fc806c1ac75f8ca7b0340d85b26
Martin Casey <x00119025@gmail.com>
2016-11-07 21:12:06 +0000

    Display user input without click

---
+++ src/app/loop-back/loop-back.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loop-back',
+  templateUrl: './loop-back.component.html',
+  styleUrls: ['./loop-back.component.css']
+})
+export class LoopBackComponent implements OnInit {
+  //Declare and initialise a String
+  message: string = "";
+
+  constructor() { }
+
+  showMe(myMessage: string)  {
+    this.message = myMessage;
+  }
+
+  ngOnInit() {
+  }
+
+}
